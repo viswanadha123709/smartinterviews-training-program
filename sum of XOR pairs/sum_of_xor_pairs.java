c2a1c4de08d3a568b30eab2df60d4cdb26493b93
viswanadha123709 <swamyviswanadh56@gmail.com>
2026-06-06 21:23:37 +0530

    Update and rename sum_of_xor_pairs.py to sum_of_xor_pairs.java

--- sum of XOR pairs/sum_of_xor_pairs.java
+++ sum of XOR pairs/sum_of_xor_pairs.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t-->0)
